dc80943dbe1e3f2d50fad64f2faffa74029e26f3
Mavnes <trudovyaroslav@yandex.ru>
2023-10-13 17:02:57 +0300

    Added read_data in main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import argparse
+import pathlib
 import sys
 from CalcRating import CalcRating
 from CalcDebt import CalcDebt
 from JsonDataReader import JsonDataReader
+from TextDataReader import TextDataReader
+from Types import DataType
 
 
 def get_path_from_arguments(args) -> str:
@@ -13,6 +16,14 @@ def get_path_from_arguments(args) -> str:
     args = parser.parse_args(args)
     return args.path
 
+def read_data(path: str) -> DataType:
+    file_extension = pathlib.Path(path).suffix
+    if file_extension == '.txt':
+        return TextDataReader().read(path)
+    elif file_extension == '.json':
+        return JsonDataReader().read(path)
+    else:
+        raise NameError('Data type is not supported.')
 
 def main():
     path = get_path_from_arguments(sys.argv[1:])
